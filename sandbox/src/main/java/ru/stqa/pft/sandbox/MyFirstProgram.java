ea5626d03db9aca56afde03247d90fc54a3f55f8
natalyapanfilova <natalyazhilova@yandex.ru>
2019-08-14 12:58:40 +0300

    Добавлены новые объекты и классы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,23 +7,22 @@ public class MyFirstProgram {
 	    hello("user");
 	    hello("Natalya");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-		double a = 5;
-		double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(6, 7);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 	}
 
 	public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double l) {
-	    return l * l;
+    public static double area(Square s) {
+	    return s.l * s.l;
     }
 
-    public static double area(double a, double b) {
-        return a * b;
+    public static double area(Rectangle r) {
+        return r.a * r.b;
     }
 }
