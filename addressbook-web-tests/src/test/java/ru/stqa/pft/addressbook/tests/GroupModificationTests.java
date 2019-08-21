c15e437416f3d08d77362f445586bd125004db50
natalyapanfilova <natalyazhilova@yandex.ru>
2019-08-21 13:05:49 +0300

    Доработано сравнение списков в GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -17,7 +18,7 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        GroupData group = new GroupData("test1", null, "test4");
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1", null, "test4");
         app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
@@ -26,6 +27,6 @@ public class GroupModificationTests extends TestBase {
 
         before.remove(before.size() - 1);
         before.add(group);
-        Assert.assertEquals(after, before);
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
 }
