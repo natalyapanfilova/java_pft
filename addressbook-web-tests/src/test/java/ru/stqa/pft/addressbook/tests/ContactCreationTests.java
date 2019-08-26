b50f4a9d29545e7075335047b955fc53bbeb307c
natalyapanfilova <natalyazhilova@yandex.ru>
2019-08-26 09:35:43 +0300

    Добавлен BeforeMethod в ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -9,28 +10,28 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+    private String groupName = "test1";
 
-    @Test
-    public void testContactCreation() {
-        String group = ensurePreconditions();
-        Contacts before = app.getContactHelper().all();
-        ContactData contact = new ContactData().withLastName("Ivanov").withFirstName("Ivan").withAddress("Rostov-on-Don, Siversa 1").withEmail("ivanov@mail.ru").withMobilePhone("89085555505").withGroup(group);
-        app.getContactHelper().createContact(contact, true);
-        app.getNavigationHelper().gotoHomePage();
-        Contacts after = app.getContactHelper().all();
-        assertThat(after.size(), equalTo(before.size() + 1));
-        assertThat(after, equalTo(before.withEdded(contact.withId(after.stream().mapToInt(ContactData::getId).max().getAsInt()))));
-    }
-
-    private String ensurePreconditions() {
+    @BeforeMethod
+    private void ensurePreconditions() {
         app.getNavigationHelper().gotoGroupPage();
-        String groupName = "test1";
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData().withName(groupName).withHeader("test2").withFooter("test3"));
         } else {
             groupName = app.getGroupHelper().getGroupName();
         }
         app.getNavigationHelper().gotoHomePage();
-        return groupName;
+    }
+
+    @Test
+    public void testContactCreation() {
+        Contacts before = app.getContactHelper().all();
+        ContactData contact =
+                new ContactData().withLastName("Ivanov").withFirstName("Ivan").withAddress("Rostov-on-Don, Siversa 1").withEmail("ivanov@mail.ru").withMobilePhone("89085555505").withGroup(groupName);
+        app.getContactHelper().createContact(contact, true);
+        app.getNavigationHelper().gotoHomePage();
+        Contacts after = app.getContactHelper().all();
+        assertThat(after.size(), equalTo(before.size() + 1));
+        assertThat(after, equalTo(before.withEdded(contact.withId(after.stream().mapToInt(ContactData::getId).max().getAsInt()))));
     }
 }
